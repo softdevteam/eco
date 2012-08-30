fd6161e7e048ba9b7ff6d88ad8df5b5b8eeaea62
l.diekmann <lukas@localhost.localdomain>
2012-08-30 16:32:03 +0100

    draw a line between nodes

--- prototype1/astviewer2.py
+++ prototype1/astviewer2.py
@@ -26,25 +26,55 @@ class Node(QGraphicsEllipseItem):
         self.right = right
         self.right.setPos(self.pos() + QPointF(50,100))
 
+    def getCenter(self):
+        return self.pos() + QPointF(25,25)
+
+class NodeLine(QGraphicsLineItem):
+
+    def __init__(self, parent, child):
+        QGraphicsLineItem.__init__(self, 0,0,0,0)
+        self.parent = parent
+        self.child = child
+
+    def paint(self, painter, option, widget):
+        x1 = self.parent.getCenter().x()
+        y1 = self.parent.getCenter().y()
+        x2 = self.child.getCenter().x()
+        y2 = self.child.getCenter().y()
+        self.setLine(QLineF(x1,y1,x2,y2))
+        QGraphicsLineItem.paint(self, painter, option, widget)
+
+
+def drawChildren(parent, parentnode):
+
+    left_node = Node(parent.left.getText())
+    parentnode.setLeft(left_node)
+    scene.addItem(left_node)
+    line = NodeLine(parentnode, left_node)
+    scene.addItem(line)
+
+    right_node = Node(parent.right.getText())
+    parentnode.setRight(right_node)
+    scene.addItem(right_node)
+    line = NodeLine(parentnode, right_node)
+    scene.addItem(line)
+
+    if isinstance(parent.left, simpleast.Expression):
+        drawChildren(parent.left, left_node)
+
+    if isinstance(parent.right, simpleast.Expression):
+        drawChildren(parent.right, right_node)
 
 app = QApplication(sys.argv)
 grview = QGraphicsView()
 scene = QGraphicsScene()
 
-p = simpleast.createTestProgram()
+p = simpleast.createTestProgram2()
 for s in p.statements:
-    st_node = Node(s.getName())
-    scene.addItem(st_node)
-    if isinstance(s, simpleast.AddExpression):
-        expr_node = Node(s.getName())
-        left_node = Node(s.left.getName())
-        right_node = Node(s.right.getName())
-        expr_node.setLeft(left_node)
-        expr_node.setRight(right_node)
-        scene.addItem(expr_node)
-        scene.addItem(left_node)
-        scene.addItem(right_node)
-
+    state_node = Node(s.getText())
+    scene.addItem(state_node)
+    if isinstance(s, simpleast.Expression):
+        drawChildren(s, state_node)
 
 grview.setScene(scene)
 
