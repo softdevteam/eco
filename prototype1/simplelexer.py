a22ed915e3ae3ea9103cd3d7ce08f0576d577e2f
l.diekmann <lukas@localhost.localdomain>
2012-08-31 15:50:32 +0100

    enhanced lexer to read assignments

--- prototype1/simplelexer.py
+++ prototype1/simplelexer.py
@@ -5,6 +5,17 @@ whitespace = " "
 endoffile = "\n"
 
 identifier = "[a-zA-Z]+"
+assignment = ":="
+
+def make_groups(**expressions):
+    regex = []
+    print(expressions)
+    for name in expressions:
+        s = "(?P<%s>%s)" % (name, expressions[name])
+        regex.append(s)
+    return r"|".join(regex)
+
+regex = make_groups(Identifier=identifier, Assignment=assignment, Whitespace=whitespace)
 
 class Token(object):
 
@@ -12,17 +23,32 @@ class Token(object):
         self.name = name
         self.value = value
 
+    def __repr__(self):
+        return "%s(%s)" % (self.name, self.value)
+
 class Lexer(object):
 
-    def __init__(self):
+    def __init__(self, code):
         self.tokens = []
+        self.code = code
+        self.pos = 0
 
-    def lex(self, code):
-        m = re.match(identifier, code)
+    def lex(self):
+        token = self.next()
+        while token is not None:
+            self.pos += len(token.value)
+            if token.name not in ["Whitespace"]:
+                self.tokens.append(token)
+            token = self.next()
+
+    def next(self):
+        m = re.match(regex, self.code[self.pos:])
         if m:
-            value = m.group(0)
-            token = Token("Identifier", value)
-            self.tokens.append(token)
+            result = m.groupdict()
+            for r in result:
+                value = result[r]
+                if value is not None:
+                    return Token(r, value)
 
 
 if __name__ == "__main__":
