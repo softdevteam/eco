09f3ee917eb98ebff2b2c3ea74a0e1ea024972c8
l.diekmann <lukas.diekmann@uni-duesseldorf.de>
2012-09-07 16:20:13 +0100

    added some more simple tests (that surprisingly work without changing the parser)

--- early/test_parser.py
+++ early/test_parser.py
@@ -6,3 +6,22 @@ def test_simple():
     assert p.rules[0].symbol == Nonterminal("E")
     assert p.rules[0].alternatives == [[Terminal("\"a\"")]]
 
+def test_multiple_symbols():
+    p = Parser("E ::= A \"a\"")
+    p.parse()
+    assert p.rules[0].alternatives == [[Nonterminal("A"), Terminal("\"a\"")]]
+
+def test_alternatives():
+    p = Parser("E ::= A | \"a\"")
+    p.parse()
+    assert p.rules[0].alternatives == [[Nonterminal("A")], [Terminal("\"a\"")]]
+
+def test_multiple_rules():
+    p = Parser("""
+        E ::= A
+        A ::= \"a\"
+    """)
+    p.parse()
+    assert p.rules[0].alternatives == [[Nonterminal("A")]]
+    assert p.rules[1].alternatives == [[Terminal("\"a\"")]]
+
