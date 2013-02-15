c54be0de6960e246b45142de801c133d71aab81b
l.diekmann <lukas.diekmann@uni-duesseldorf.de>
2013-02-15 15:48:36 +0000

    fix: identifiers containing keywords

--- token_lexer.py
+++ token_lexer.py
@@ -14,7 +14,11 @@ class TokenLexer(object):
                 m = re.match(regex, remaining)
                 if m:
                     result = m.group(0)
-                    if len(result) >= len(longest_match[0]):
+                    if len(result) > len(longest_match[0]):
+                        new_priority = self.regexlist[regex][0]
+                        regex_name = self.regexlist[regex][1]
+                        longest_match = (result, regex_name, new_priority)
+                    if len(result) == len(longest_match[0]):
                         new_priority = self.regexlist[regex][0]
                         old_priority = longest_match[2]
                         if new_priority < old_priority: # use token with higher priority (smaller numbers have higher priority)
