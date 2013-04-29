ed624a5aae31e6ca8bfe56fcede8f6a2fa29e6f8
Laurence Tratt <laurie@tratt.net>
2013-04-29 11:25:21 +0200

    Build a simple grammar parser.
    This is needed for more complex grammar operations.

--- node_editor/gops.py
+++ node_editor/gops.py
@@ -4,18 +4,67 @@ import re
 from languages import Language
 
 
+
+
+def _rules_to_str(rules):
+    s = []
+    for r in rules:
+        s.extend([r[0], "::="])
+        i = 0
+        for alt in r[1:]:
+            if i > 0:
+                s.append("|")
+            i += 1
+            s.extend(alt)
+        s.append("\n")
+    return " ".join(s)
+
+
+RE_RULE = re.compile(r"([^\s]+)\s*::=\s*")
+RE_WORD = re.compile(r"([^\s|]+)")
+
+def _parse_grm(grm):
+    rules = []
+    i = 0
+
+    def _skip_ws(i):
+        while i < len(grm) and grm[i] in " \t\r\n":
+            i += 1
+        return i
+
+    while i < len(grm):
+        i = _skip_ws(i)
+        if i == len(grm):
+            break
+        m = RE_RULE.match(grm, i)
+        assert m is not None
+        rule = [m.group(1)]
+        alt  = []
+        i = m.end()
+        while i < len(grm):
+            i = _skip_ws(i)
+            if i == len(grm):
+                break
+            if RE_RULE.match(grm, i):
+                break
+            if grm[i] == "|":
+                rule.append(alt)
+                alt = []
+                i += 1
+                continue
+            m = RE_WORD.match(grm, i)
+            assert m is not None
+            alt.append(m.group(1))
+            i = m.end()
+        rule.append(alt)
+        rules.append(rule)
+
+    return rules
+
+
 def add_alt(new_name, old_lang, rule, alt):
-    old_grm = old_lang.grammar
-    m = re.search(r"%s\s*::=" % rule, old_grm)
-    assert m is not None
-    rest = old_grm[m.end():]
-    if rest.find("::=") == -1:
-        btwn = rest
-    else:
-        btwn = old_grm[m.end():rest.find("::=")]
-    btwn = btwn.strip()
-    if btwn == "":
-        assert False
-    else:
-        new_grm = "".join([old_grm[:m.end()], "%s | " % alt, old_grm[m.end():]])
-    return Language(new_name, new_grm, old_lang.priorities)
+    rules = _parse_grm(old_lang.grammar)
+    for r in rules:
+        if r[0] == rule:
+            r.append([alt])
+    return Language(new_name, _rules_to_str(rules), old_lang.priorities)
