38128f4abc14a69ead87164f97d333fd67e3cdd0
Lukas Diekmann <lukas.diekmann@uni-duesseldorf.de>
2013-09-30 12:29:14 +0100

    added python exmaples; moved java examples

---
+++ python_examples/many_indents.py
@@ -0,0 +1,10 @@
+class X(object):
+    def test():
+        return [(test, '', 0)]
+
+    def relex(self, startnode):
+        # XXX: comment 1
+        # comment with (brackets) in it
+
+        # comment after space with an ' apostrophe
+        pass
