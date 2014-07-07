a2aa8bed4000762c67f70c77820231605e6f63ee
Lukas Diekmann <lukas.diekmann@gmail.com>
2014-07-07 11:44:05 +0100

    added PHP-Python exporter

---
+++ lib/eco/export/PHPPython.py
@@ -0,0 +1,19 @@
+import helper
+
+class PHP(helper.Generic):
+    def language_box(self, name, node):
+        if name == "Python":
+            buf = Python().pp(node)
+            self.buf.append("embed_py_func(\"\"\"%s\"\"\")" % (_escape(buf)))
+
+class Python(helper.Generic):
+    def language_box(self, name, node):
+        if name == "PHP":
+            buf = Python().pp(node)
+            self.buf.append("embed_php_func(%s)" % (_escape(buf)))
+
+def _escape(s):
+    return s.replace("\"", "\\\"").replace("'", "\\'").replace("\\", "\\\\")
+
+def export(node):
+    return PHP().pp(node)
