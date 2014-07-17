7d597e7d9c1c3f89d92338c0f3f404f1a44d0f39
Lukas Diekmann <lukas.diekmann@gmail.com>
2014-07-17 12:46:42 +0100

    delete old files

--- lib/eco/gui.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'newgui.ui'
-#
-# Created: Wed Nov  6 14:19:57 2013
-#      by: PyQt4 UI code generator 4.10.3
-#
-# WARNING! All changes made in this file will be lost!
-
