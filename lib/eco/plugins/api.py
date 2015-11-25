eeb48d60e392bef9a2a153e89c7ea3558fd70ffc
Sarah Mount <sarah.mount@kcl.ac.uk>
2015-11-25 11:41:33 +0000

    Give plugins access to basic TreeManager methods.

--- lib/eco/plugins/api.py
+++ lib/eco/plugins/api.py
@@ -11,6 +11,44 @@ class Plugin(object):
         self.language = language
         self.name = name
         self._activated = True
+        self._tm = None  # TreeManager must be set (per document) by the GUI.
+
+    @abc.abstractmethod
+    def run_tool(self):
+        """Run an external tool and annotate nodes.
+        """
+        raise NotImplementedError("Must be overridden in subclass.")
+
+    def export(self, path=None):
+        return self._tm.export(path)
+
+    def export_as_text(self, path=None):
+        return self._tm.export_as_text(path)
+
+    def get_node_at_pos(self, lineno, charno=0):
+        """Get the node at a given position.
+        If charno is not given, this method returns the first non-whitespace
+        node on the given line.
+        """
+        temp_cursor = self.tm.cursor.copy()
+        temp_cursor.line = lineno - 1
+        temp_cursor.move_to_x(charno, self.tm.lines)
+        node = temp_cursor.find_next_visible(temp_cursor.node)
+        if node.lookup == "<ws>":
+            node = node.next_term
+        return temp_cursor.find_next_visible(temp_cursor.node)
+
+    @property
+    def tm(self):
+        """The TreeManager object needs to be set by external code.
+        Ideally we want to hide the TreeManager and other Eco internals
+        from plugins.
+        """
+        return self._tm
+
+    @tm.setter
+    def tm(self, tm):
+        self._tm = tm
 
     @property
     def activated(self):
@@ -18,16 +56,14 @@ class Plugin(object):
 
     def activate(self):
         """Activate this plugin.
+        i.e. show it as checked in the GUI and run the external tool for
+        which this plugin is a proxy.
         """
         self._activated = True
 
     def deactivate(self):
         """Deactivate this plugin.
+        i.e. show it as unchecked in the GUI and do not run the external tool
+        for which this plugin is a proxy.
         """
         self._activated = False
-
-    @abc.abstractmethod
-    def run_tool(self):
-        """Run an external tool and annotate nodes.
-        """
-        raise NotImplementedError("Must be overridden in subclass.")
