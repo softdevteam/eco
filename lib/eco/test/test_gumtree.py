d2f7dca0cad2e77c6d1434ccaa4dfe2d290b5f53
Geoffrey French <frondit1985@gmail.com>
2015-12-10 14:30:20 +0000

    Gumtree driver working. Gumtree tests now use Python `ast` module to compile code to an AST, which is converted to a tree for processing in Gumtree.

--- lib/eco/test/test_gumtree.py
+++ lib/eco/test/test_gumtree.py
@@ -1,3 +1,4 @@
+import ast
 import pytest
 
 from version_control.gumtree_driver import GumTreeMerger, gumtree_swingdiff, EcoTreeDocument as Doc,\
@@ -6,107 +7,121 @@ from version_control.gumtree_driver import GumTreeMerger, gumtree_swingdiff, Eco
     GumtreeDiffMove as DMove
 
 
+class ASTConverter (object):
+    def __init__(self):
+        self._node_classes = {}
+
+    def _get_node_class(self, ast_node):
+        t = type(ast_node)
+        try:
+            return self._node_classes[t]
+        except KeyError:
+            cls = NodeClass(len(self._node_classes), t.__name__)
+            self._node_classes[t] = cls
+            return cls
+
+    def _handle_ast_value(self, children, values, x):
+        if isinstance(x, ast.AST):
+            children.append(self._ast_to_tree(x))
+        elif isinstance(x, (list, tuple)):
+            for v in x:
+                self._handle_ast_value(children, values, v)
+        else:
+            values.append(x)
+
+
+    def _ast_to_tree(self, ast_node):
+        children = []
+        values = []
+        for field_name in ast_node._fields:
+            field_val = getattr(ast_node, field_name)
+            self._handle_ast_value(children, values, field_val)
+        node_class = self._get_node_class(ast_node)
+        value = '_'.join([str(x) for x in values])
+        return node_class.node(value, children)
+
+    def parse(self, code):
+        a = ast.parse(code)
+        return self._ast_to_tree(a)
+
+
 class Test_gumtree:
     def test_same(self):
-        A = NodeClass(1, 'A')
-        B = NodeClass(2, 'B')
-        C = NodeClass(3, 'C')
-        T0 = A.node('a', [])
-        T1 = A.node('a', [])
+        conv = ASTConverter()
+        T0 = conv.parse('a')
+        T1 = conv.parse('a')
         assert GumTreeMerger(T0).diff(T1) == []
 
-        T0 = A.node('a', [B.node('b', [])])
-        T1 = A.node('a', [B.node('b', [])])
+        T0 = conv.parse('a+b')
+        T1 = conv.parse('a+b')
         assert GumTreeMerger(T0).diff(T1) == []
 
-        T0 = A.node('a', [B.node('b', []), C.node('c', [])])
-        T1 = A.node('a', [B.node('b', []), C.node('c', [])])
+        T0 = conv.parse('[a, b]')
+        T1 = conv.parse('[a, b]')
         assert GumTreeMerger(T0).diff(T1) == []
 
 
     def test_update(self):
-        A = NodeClass(1, 'A')
-        B = NodeClass(2, 'B')
-        C = NodeClass(3, 'C')
+        conv = ASTConverter()
+        T0 = conv.parse('a+b')
+        T1 = conv.parse('a+x')
+        assert GumTreeMerger(T0).diff(T1) == [DUpdate(T0[0][0][2], 'x')]
 
-        # A(a) - B(b)  ==>  A(a) - B(x)
-        T0 = A.node('a', [B.node('b', [])])
-        T1 = A.node('a', [B.node('x', [])])
-        assert GumTreeMerger(T0).diff(T1) == [DUpdate(None, T0[0], 'x')]
+        T0 = conv.parse('[a, b]')
+        T1 = conv.parse('[x, y]')
+        assert GumTreeMerger(T0).diff(T1) == [DUpdate(T0[0][0][0], 'x'),
+                                              DUpdate(T0[0][0][1], 'y')]
 
-        # A(a) - B(b),C(c)  ==>  A(a) - B(x),C(y)
-        T0 = A.node('a', [B.node('b', []), C.node('c', [])])
-        T1 = A.node('a', [B.node('x', []), C.node('y', [])])
-        assert GumTreeMerger(T0).diff(T1) == [DUpdate(None, T0[0], 'x'), DUpdate(None, T0[1], 'y')]
-
-        # A(a) - B(b) - C(c)  ==>  A(a) - B(x) - C(c)
-        T0 = A.node('a', [B.node('b', [C.node('c', [])])])
-        T1 = A.node('a', [B.node('x', [C.node('c', [])])])
-        assert GumTreeMerger(T0).diff(T1) == [DUpdate(None, T0[0], 'x')]
+        T0 = conv.parse('[a, b+c]')
+        T1 = conv.parse('[a, x+y]')
+        assert GumTreeMerger(T0).diff(T1) == [DUpdate(T0[0][0][1][0], 'x'),
+                                              DUpdate(T0[0][0][1][2], 'y')]
 
 
     def test_insert(self):
-        A = NodeClass(1, 'A')
-        B = NodeClass(2, 'B')
-        C = NodeClass(3, 'C')
+        conv = ASTConverter()
+        T0 = conv.parse('[a, b]')
+        T1 = conv.parse('[a, c, b]')
+        assert GumTreeMerger(T0).diff(T1) == [DInsert(T1[0][0][1], T0[0][0], 1),
+                                              DInsert(T1[0][0][1][0], T1[0][0][1], 0)]
 
-        # A(a) - B(b)  ==>  A(a) - B(b) - C(c)
-        T0 = A.node('a', [B.node('b', [])])
-        T1 = A.node('a', [B.node('b', [C.node('c', [])])])
-        assert GumTreeMerger(T0).diff(T1) == [DInsert(None, None, T0[0], 0, T1[0][0])]
-
-        # A(a) - B(b)  ==>  A(a) - B(b) - C(c) - A(x)
-        T0 = A.node('a', [B.node('b', [])])
-        T1 = A.node('a', [B.node('b', [C.node('c', [A.node('x', [])])])])
-        assert GumTreeMerger(T0).diff(T1) == [DInsert(None, None, T0[0], 0, T1[0][0]),
-                                   DInsert(None, None, T1[0][0], 0, T1[0][0][0])]
+        T0 = conv.parse('[a, b]')
+        T1 = conv.parse('[a, c+d, b]')
+        assert GumTreeMerger(T0).diff(T1) == [DInsert(T1[0][0][1], T0[0][0], 1),
+                                              DInsert(T1[0][0][1][0], T1[0][0][1], 0),
+                                              DInsert(T1[0][0][1][1], T1[0][0][1], 1),
+                                              DInsert(T1[0][0][1][2], T1[0][0][1], 2),
+                                              DInsert(T1[0][0][1][0][0], T1[0][0][1][0], 0),
+                                              DInsert(T1[0][0][1][2][0], T1[0][0][1][2], 0)]
 
 
-    # def test_replace(self):
-    #     assert diff3(['b'], ['a'], ['a']) == [Diff3ConflictRegion(base=['b'], derived_main=['a'])]
-    #     assert diff3(['b'], ['a'], ['a'], True) == ['a']
-    #     assert diff3(['a'], ['b'], ['a']) == ['b']
-    #     assert diff3(['a'], ['a'], ['b']) == ['b']
-    #     assert diff3(['a', 'b'], ['c', 'b'], ['c', 'b']) == [Diff3ConflictRegion(base=['a'], derived_main=['c']), 'b']
-    #     assert diff3(['a', 'b'], ['c', 'b'], ['c', 'b'], True) == ['c', 'b']
-    #     assert diff3(['c', 'b'], ['a', 'b'], ['c', 'b']) == ['a', 'b']
-    #     assert diff3(['c', 'b'], ['c', 'b'], ['a', 'b']) == ['a', 'b']
-    #     assert diff3(['a'], ['b'], ['c']) == [Diff3ConflictRegion(base=['a'], derived_local=['b'], derived_main=['c'])]
-    #
-    #     assert diff3(['a', 'b', 'c'], ['a', 'b', 'c'], ['a', 'b', 'c']) == ['a', 'b', 'c']
-    #     assert diff3(['a', 'b', 'c'], ['a', 'd', 'c'], ['a', 'b', 'c']) == ['a', 'd', 'c']
-    #     assert diff3(['a', 'b', 'c'], ['a', 'b', 'c'], ['a', 'd', 'c']) == ['a', 'd', 'c']
-    #     assert diff3(['a', 'b', 'c'], ['a', 'e', 'c'], ['a', 'd', 'c']) == ['a', Diff3ConflictRegion(base=['b'], derived_local=['e'], derived_main=['d']), 'c']
-    #     assert diff3(['a', 'b', 'c'], ['a', 'b', 'e', 'c'], ['a', 'x', 'c']) == ['a', Diff3ConflictRegion(base=['b'], derived_local=['b', 'e'], derived_main=['x']), 'c']
-    #
-    # def test_insert(self):
-    #     assert diff3(['x', 'a', 'b'], ['a', 'b'], ['a', 'b']) == [Diff3ConflictRegion(base=['x'], derived_main=[]), 'a', 'b']
-    #     assert diff3(['x', 'a', 'b'], ['a', 'b'], ['a', 'b'], True) == ['a', 'b']
-    #     assert diff3(['a', 'b'], ['x', 'a', 'b'], ['a', 'b']) == ['x', 'a', 'b']
-    #     assert diff3(['a', 'b'], ['a', 'b'], ['x', 'a', 'b']) == ['x', 'a', 'b']
-    #     assert diff3(['a', 'x', 'b'], ['a', 'b'], ['a', 'b']) == ['a', Diff3ConflictRegion(base=['x'], derived_main=[]), 'b']
-    #     assert diff3(['a', 'x', 'b'], ['a', 'b'], ['a', 'b'], True) == ['a', 'b']
-    #     assert diff3(['a', 'b'], ['a', 'x', 'b'], ['a', 'b']) == ['a', 'x', 'b']
-    #     assert diff3(['a', 'b'], ['a', 'b'], ['a', 'x', 'b']) == ['a', 'x', 'b']
-    #     assert diff3(['a', 'b', 'x'], ['a', 'b'], ['a', 'b']) == ['a', 'b', Diff3ConflictRegion(base=['x'], derived_main=[])]
-    #     assert diff3(['a', 'b', 'x'], ['a', 'b'], ['a', 'b'], True) == ['a', 'b']
-    #     assert diff3(['a', 'b'], ['a', 'b', 'x'], ['a', 'b']) == ['a', 'b', 'x']
-    #     assert diff3(['a', 'b'], ['a', 'b'], ['a', 'b', 'x']) == ['a', 'b', 'x']
-    #     assert diff3(['a', 'b'], ['a', 'b', 'x'], ['a', 'b', 'c']) == ['a', 'b', Diff3ConflictRegion(base=[], derived_local=['x'], derived_main=['c'])]
-    #
-    # def test_remove(self):
-    #     assert diff3(['a', 'b'], ['x', 'a', 'b'], ['x', 'a', 'b']) == [Diff3ConflictRegion(base=[], derived_main=['x']), 'a', 'b']
-    #     assert diff3(['a', 'b'], ['x', 'a', 'b'], ['x', 'a', 'b'], True) == ['x', 'a', 'b']
-    #     assert diff3(['x', 'a', 'b'], ['a', 'b'], ['x', 'a', 'b']) == ['a', 'b']
-    #     assert diff3(['x', 'a', 'b'], ['x', 'a', 'b'], ['a', 'b']) == ['a', 'b']
-    #     assert diff3(['a', 'b'], ['a', 'x', 'b'], ['a', 'x', 'b']) == ['a', Diff3ConflictRegion(base=[], derived_main=['x']), 'b']
-    #     assert diff3(['a', 'b'], ['a', 'x', 'b'], ['a', 'x', 'b'], True) == ['a', 'x', 'b']
-    #     assert diff3(['a', 'x', 'b'], ['a', 'b'], ['a', 'x', 'b']) == ['a', 'b']
-    #     assert diff3(['a', 'x', 'b'], ['a', 'x', 'b'], ['a', 'b']) == ['a', 'b']
-    #     assert diff3(['a', 'b'], ['a', 'b', 'x'], ['a', 'b', 'x']) == ['a', 'b', Diff3ConflictRegion(base=[], derived_main=['x'])]
-    #     assert diff3(['a', 'b'], ['a', 'b', 'x'], ['a', 'b', 'x'], True) == ['a', 'b', 'x']
-    #     assert diff3(['a', 'b', 'x'], ['a', 'b'], ['a', 'b', 'x']) == ['a', 'b']
-    #     assert diff3(['a', 'b', 'x'], ['a', 'b', 'x'], ['a', 'b']) == ['a', 'b']
-    #     assert diff3(['a', 'b', 'x'], ['a', 'b'], ['a', 'b', 'c']) == ['a', 'b', Diff3ConflictRegion(base=['x'], derived_main=['c'], derived_local=[])]
-    #     assert diff3(['a', 'b', 'x'], ['a', 'b', 'c'], ['a', 'b']) == ['a', 'b', Diff3ConflictRegion(base=['x'], derived_main=[], derived_local=['c'])]
-    #
+    def test_delete(self):
+        conv = ASTConverter()
+        T0 = conv.parse('[a, c, b]')
+        T1 = conv.parse('[a, b]')
+        assert GumTreeMerger(T0).diff(T1) == [DDelete(T0[0][0][1][0]),
+                                              DDelete(T0[0][0][1])]
+
+        T0 = conv.parse('[a, c+d, b]')
+        T1 = conv.parse('[a, b]')
+        assert GumTreeMerger(T0).diff(T1) == [DDelete(T0[0][0][1][0][0]),
+                                              DDelete(T0[0][0][1][0]),
+                                              DDelete(T0[0][0][1][1]),
+                                              DDelete(T0[0][0][1][2][0]),
+                                              DDelete(T0[0][0][1][2]),
+                                              DDelete(T0[0][0][1])]
+
+
+    def test_move(self):
+        conv = ASTConverter()
+        T0 = conv.parse('[a, x(b, c+d(e)/f+g, h), i, y(j, k+l(m)/n+o, p), q]')
+        T1 = conv.parse('[a, x(b, c+d(e)/f+g, y(j, k+l(m)/n+o, p), h), i, q]')
+        assert GumTreeMerger(T0).diff(T1) == [DMove(T0[0][0][3], T1[0][0][1], 3)]
+
+
+    def test_all(self):
+        conv = ASTConverter()
+        T0 = conv.parse('[a, x(b, c+d(e)/f+g, h), i, y(j, k+l(m)/n+o, p), q]')
+        T1 = conv.parse('[a, x(b, c+d(e)/f+g, y(j, k+l(m)/n+o, p), r), i, q]')
+        assert GumTreeMerger(T0).diff(T1) == [DMove(T0[0][0][3], T1[0][0][1], 3),
+                                              DUpdate(T0[0][0][1][3], 'r')]
