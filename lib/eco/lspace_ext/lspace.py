d3a203dcde45c3865277b1457dcc3bac1d240728
Geoffrey French <frondit1985@gmail.com>
2016-01-15 17:21:29 +0000

    Modified handling of LSpace viewer style sheets to account for changes in tool.

--- lib/eco/lspace_ext/lspace.py
+++ lib/eco/lspace_ext/lspace.py
@@ -20,12 +20,49 @@ def _get_lspace_viewer_path(lspace_root=None):
 
 
 
+def _coerce_js(js):
+    if js is None or isinstance(js, (str, unicode, int, long, float, bool)):
+        return js
+    elif isinstance(js, (tuple, list)):
+        return [_coerce_js(x) for x in js]
+    elif isinstance(js, dict):
+        return {key: _coerce_js(value) for key, value in js.items()}
+    else:
+        try:
+            as_json = getattr(js, 'as_json', None)
+        except AttributeError:
+            raise TypeError('Cannot coerce {0} to JSON'.format(type(js)))
+        else:
+            return _coerce_js(as_json())
+
 class JSONData (object):
     def __init__(self, **js):
         self.js = js
 
     def as_json(self):
         return self.js
+
+
+
+class TextWeight (object):
+    normal = 'normal'
+    bold = 'bold'
+
+    @staticmethod
+    def check(x):
+        if x not in {TextWeight.normal, TextWeight.bold}:
+            raise ValueError('Invalid text weight {0}'.format(x))
+        return x
+
+class TextSlant (object):
+    normal = 'normal'
+    italic = 'italic'
+
+    @staticmethod
+    def check(x):
+        if x not in {TextSlant.normal, TextSlant.italic}:
+            raise ValueError('Invalid text slant {0}'.format(x))
+        return x
 
 
 class FlowIndent (JSONData):
@@ -40,6 +77,11 @@ class FlowIndent (JSONData):
     @staticmethod
     def except_first(indent):
         return FlowIndent(indent_type='except_first', indent=indent)
+
+
+class Colour (JSONData):
+    def __init__(self, r=0.0, g=0.0, b=0.0, a=1.0):
+        super(Colour, self).__init__(r=r, g=g, b=b, a=a)
 
 
 
@@ -62,24 +104,27 @@ class Text (Pres):
 
 
 class Column (Pres):
-    def __init__(self, children, y_spacing=0.0):
-        super(Column, self).__init__(__type__='Column', y_spacing=y_spacing,
+    def __init__(self, children):
+        super(Column, self).__init__(__type__='Column',
                 children=[Pres.coerce(c).as_json() for c in children])
 
 
 class Row (Pres):
-    def __init__(self, children, x_spacing=0.0):
-        super(Row, self).__init__(__type__='Row', x_spacing=x_spacing,
+    def __init__(self, children):
+        super(Row, self).__init__(__type__='Row',
                 children=[Pres.coerce(c).as_json() for c in children])
 
 
 class Flow (Pres):
-    def __init__(self, children, x_spacing=0.0, y_spacing=0.0, indentation=None):
-        if indentation is None:
-            indentation = FlowIndent.no_indent()
-        super(Flow, self).__init__(__type__='Flow', x_spacing=x_spacing,
-                y_spacing=y_spacing, indentation=indentation.as_json(),
+    def __init__(self, children):
+        super(Flow, self).__init__(__type__='Flow',
                 children=[Pres.coerce(c).as_json() for c in children])
+
+
+class ApplyStyleSheet (Pres):
+    def __init__(self, child, **kwargs):
+        super(ApplyStyleSheet, self).__init__(__type__='ApplyStyleSheet',
+                child=Pres.coerce(child).as_json(), **kwargs)
 
 
 def viewer(pres, lspace_root=None):
@@ -102,6 +147,6 @@ def viewer(pres, lspace_root=None):
             "{0}, you can set the LSpace root using the settings "
             "dialog".format(lspace_viewer_path))
 
-    json.dump(Pres.coerce(pres).as_json(), proc.stdin)
+    json.dump(_coerce_js(Pres.coerce(pres)), proc.stdin)
     proc.stdin.close()
 
