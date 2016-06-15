47e3d4b435bf789e3c2d29c67215f8abc0fab3c5
Sarah Mount <sarah.mount@kcl.ac.uk>
2016-06-15 16:39:40 +0100

    Dummy parser for Ruby.

--- lib/eco/rubyparser/rubyparser.py
+++ lib/eco/rubyparser/rubyparser.py
@@ -1,6 +1,10 @@
+from export.jruby import JRubyExporter
 from incparser.astree import AST, BOS, EOS, TextNode
 from grammar_parser.gparser import Terminal, Nonterminal
 from incparser.syntaxtable import FinishSymbol
+
+import subprocess
+import tempfile
 
 class RubyLexer(object):
     def relex(self, node):
@@ -9,12 +13,21 @@ class RubyLexer(object):
     def is_indentation_based(self):
         return False
 
+class LineDummy(object):
+    def __init__(self, node):
+        self.node = node
+
+class DummyTM(object):
+    def __init__(self, node):
+        self.lines = [LineDummy(node)]
+
 class RubyParser(object):
     def __init__(self):
         self.previous_version = None
         self.error_node = None
         self.graph = None
         self.last_status = True
+        self.whitespaces = True
 
     def init_ast(self, magic_parent = None):
         bos = BOS(Terminal(""), 0, [])
@@ -26,9 +39,40 @@ class RubyParser(object):
         root = TextNode(Nonterminal("Root"), 0, [bos, eos])
         self.previous_version = AST(root)
 
+    def _find_node(self, lineno, charno):
+        node = self.previous_version.parent.children[0]
+        current_line = 1
+        current_charno = 0
+        while True:
+            node = node.next_term
+            if isinstance(node, EOS):
+                return node
+            if node.lookup == "<return>":
+                current_line += 1
+                current_charno = 0
+                continue
+            current_charno += len(node.symbol.name)
+            if current_line == lineno and current_charno >= charno:
+                return node
+
     def inc_parse(self, line_indents = [], reparse=False):
-        # XXX replace root with result of jruby parse
-        pass
+        self.last_status = True
+        self.error_node = None
+        exporter = JRubyExporter(DummyTM(self.previous_version.parent.children[0]))
+        f = tempfile.mkstemp(suffix=".rb")
+        exporter.export(path=f[1])
+        proc = subprocess.Popen(["ruby-parse", f[1]],
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE)
+        stdout, _ = proc.communicate()
+        for line in stdout.split('\n'):
+            if line.startswith('warning:'):
+                continue
+            if line.startswith(f[1]):
+                tokens = line.split(':')
+                self.error_node = self._find_node(int(tokens[1]), int(tokens[2]))
+                self.last_status = False
+                return
 
     def save_status(self, version):
         pass
