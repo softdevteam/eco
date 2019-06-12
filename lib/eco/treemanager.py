f781eee38154fcbfb41a40eb7568ac4ccbdcb925
Lukas Diekmann <lukas.diekmann@gmail.com>
2019-06-13 00:07:13 +0100

    Fix bug when shrinking language boxes
    Behave correctly when encountering multitext nodes while shrinking
    language boxes, i.e. copy all of the multitext nodes' contents so that
    it can be pasted outside of the box.

--- lib/eco/treemanager.py
+++ lib/eco/treemanager.py
@@ -1569,9 +1569,20 @@ class TreeManager(object):
         l = []
         n = newend
         while type(n) is not EOS:
-            l.append(n.symbol.name)
+            if isinstance(n, MultiTextNode):
+                for c in list(n.children):
+                    l.append(c.symbol.name)
+                    c.remove()
+                    c.deleted = True
+            else:
+                l.append(n.symbol.name)
             n.remove()
             n = n.next_term
+
+        for i in reversed(range(len(self.lines))):
+            if self.lines[i].node.deleted:
+                del self.lines[i]
+
         self.skipautolbox = True
         self.reparse(lbox.symbol.ast.children[0], True)
 
